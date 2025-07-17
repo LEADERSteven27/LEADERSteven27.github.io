8d588fb121b2a886e5b7dd312f75dfcd277ff1cf
LEADERSteven27 <2964902303@qq.com>
2025-07-17 22:55:22 +0800

    Site updated: 2025-07-17 22:55:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/14/hello-world/"];function toRandomPost(){
+var posts=["2025/07/14/hello-world/","2025/07/15/动物朋友/","2025/07/17/微观经济学/","2025/07/15/文献综述与科技写作/","2025/07/15/计算机视觉/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
